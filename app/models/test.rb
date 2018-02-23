e6d4f74062e34a18440356fd8b4512da2734cb29
OnlyReFLeX <reflexonly@gmail.com>
2018-02-23 21:47:56 +0300

    add find category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  def self.category_by_title(title)
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+        .where('categories.title = ?', title)
+        .order('title DESC')
+  end
 end
