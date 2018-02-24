5a8a3bf4c59d20c9452263f564c8a16ee8692fcd
OnlyReFLeX <reflexonly@gmail.com>
2018-02-24 23:38:19 +0300

    elastic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,64 +6,64 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create([
+categories = Category.create([
   { title: 'Математика' },
   { title: 'Информатика' }
 ])
 
-Test.create([
-  { title: 'Вычитание', level: 1, category_id: 1 },
-  { title: 'Сложение', category_id: 1 },
-  { title: 'Части компьютера', level: 2, category_id: 2 }
+tests = Test.create([
+  { title: 'Вычитание', level: 1, category_id: categories[0].id },
+  { title: 'Сложение', category_id: categories[0].id },
+  { title: 'Части компьютера', level: 2, category_id: categories[1].id }
 ])
 
-Question.create([
-  { body: '5+5?', test_id: 1 },
-  { body: '3+2?', test_id: 1 },
-  { body: '4-2?', test_id: 2 },
-  { body: '5-1?', test_id: 2 },
-  { body: 'Что такое HDD?', test_id: 3 },
-  { body: 'Что такое ОЗУ?', test_id: 3 }
+questions = Question.create([
+  { body: '5+5?', test_id: tests[0].id },
+  { body: '3+2?', test_id: tests[0].id },
+  { body: '4-2?', test_id: tests[1].id },
+  { body: '5-1?', test_id: tests[1].id },
+  { body: 'Что такое HDD?', test_id: tests[2].id },
+  { body: 'Что такое ОЗУ?', test_id: tests[2].id }
 ])
 
 Answer.create([
-  { body: '8', question_id: 1 },
-  { body: '6', question_id: 1 },
-  { body: '7', question_id: 1 },
-  { body: '10', correct: true, question_id: 1 },
+  { body: '8', question_id: questions[0].id },
+  { body: '6', question_id: questions[0].id },
+  { body: '7', question_id: questions[0].id },
+  { body: '10', correct: true, question_id: questions[0].id },
 
-  { body: '5', correct: true, question_id: 2 },
-  { body: '1', question_id: 2 },
-  { body: '4', question_id: 2 },
-  { body: '2', question_id: 2 },
+  { body: '5', correct: true, question_id: questions[1].id },
+  { body: '1', question_id: questions[1].id },
+  { body: '4', question_id: questions[1].id },
+  { body: '2', question_id: questions[1].id },
 
-  { body: '6', question_id: 3 },
-  { body: '2', correct: true, question_id: 3 },
-  { body: '5', question_id: 3 },
-  { body: '3', question_id: 3 },
+  { body: '6', question_id: questions[2].id },
+  { body: '2', correct: true, question_id: questions[2].id },
+  { body: '5', question_id: questions[2].id },
+  { body: '3', question_id: questions[2].id },
 
-  { body: '8', question_id: 4 },
-  { body: '5', question_id: 4 },
-  { body: '4', correct: true, question_id: 4 },
-  { body: '6', question_id: 4 },
+  { body: '8', question_id: questions[3].id },
+  { body: '5', question_id: questions[3].id },
+  { body: '4', correct: true, question_id: questions[3].id },
+  { body: '6', question_id: questions[3].id },
 
-  { body: 'Монитор', question_id: 5 },
-  { body: 'Жесткий диск', correct: true, question_id: 5 },
-  { body: 'Мышь', question_id: 5 },
-  { body: 'Процессор', question_id: 5 },
+  { body: 'Монитор', question_id: questions[4].id },
+  { body: 'Жесткий диск', correct: true, question_id: questions[4].id },
+  { body: 'Мышь', question_id: questions[4].id },
+  { body: 'Процессор', question_id: questions[4].id },
 
-  { body: 'Оперативная Память', correct: true, question_id: 6 },
-  { body: 'Пиксели в мониторе', question_id: 6 },
-  { body: 'Видео карта', question_id: 6 },
-  { body: 'Блок питания', question_id: 6 }
+  { body: 'Оперативная Память', correct: true, question_id: questions[5].id },
+  { body: 'Пиксели в мониторе', question_id: questions[5].id },
+  { body: 'Видео карта', question_id: questions[5].id },
+  { body: 'Блок питания', question_id: questions[5].id }
 ])
 
-User.create([
+users = User.create([
   { login: 'user1', password: 'password1' },
   { login: 'user2', password: 'password2' }
 ])
 
 UsersTest.create([
-  { user_id: 1, test_id: 2 },
-  { user_id: 1, test_id: 3 }
+  { user_id: users[0], test_id: tests[1] },
+  { user_id: users[0], test_id: tests[2] }
 ])
