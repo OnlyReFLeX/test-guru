22df5267d1da896b6d790c9fe92293df099089e2
OnlyReFLeX <reflexonly@gmail.com>
2018-02-26 00:10:34 +0300

    refactoring assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_user
   has_many :tests, through: :tests_user
-  has_many :my_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :passed_tests, class_name: 'Test', foreign_key: 'author_id'
 
   def open_tests_by_level(level)
     Test.joins('JOIN users_tests ON users_tests.test_id = tests.id')
