f44aa1ef677efcec9be3412dc14d33ab8c46781e
OnlyReFLeX <reflexonly@gmail.com>
2018-02-25 20:19:17 +0300

    add assoc

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :tests_user
+  has_many :users, through: :tests_user
+  belongs_to :author, class_name: 'User'
+
   def self.category_by_title(title)
     Test.joins('JOIN categories ON tests.category_id = categories.id')
         .where(categories: { title: title })
