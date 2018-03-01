c38bf5430b470ae2f2451d075b84957542b4de17
OnlyReFLeX <reflexonly@gmail.com>
2018-03-01 20:18:55 +0300

    Просмотр конкретного вопроса теста

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,18 @@
 class QuestionsController < ApplicationController
+  before_action :find_question, only: %i[show destroy]
+
   def index
     @questions = Question.where(test_id: params[:test_id])
     render inline:  "<% @questions.each do |q| %><p><%= q.body %></p><% end %>"
+  end
+
+  def show
+    render inline: @question.body
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
