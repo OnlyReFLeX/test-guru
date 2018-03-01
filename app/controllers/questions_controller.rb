8703ed85b1ed910c3e1c7b4d19b8553db9dc2996
OnlyReFLeX <reflexonly@gmail.com>
2018-03-01 20:16:02 +0300

    Просмотр списка вопросов теста

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,6 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.where(test_id: params[:test_id])
+    render inline:  "<% @questions.each do |q| %><p><%= q.body %></p><% end %>"
+  end
 end
