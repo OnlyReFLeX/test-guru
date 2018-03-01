dc372f38c34ca7c9049c53a1db9bdfe631057817
OnlyReFLeX <reflexonly@gmail.com>
2018-03-01 23:52:12 +0300

    add rescue with not found

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy]
   before_action :find_test, only: %i[index create]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     @questions = Question.where(test_id: @test_id)
@@ -8,7 +9,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    render inline: @question.body
+    render plain: @question.body
   end
 
   def new
@@ -16,7 +17,7 @@ class QuestionsController < ApplicationController
 
   def create
     question = Question.create(body: question_params[:body], test_id: @test_id)
-    render inline: @question.body
+    render plain: question.body
   end
 
   def destroy
@@ -35,5 +36,9 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Ресурс не найден.'
   end
 end
