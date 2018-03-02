ae27b64aa29fa66723e4efa6d3809e6b904ad85a
OnlyReFLeX <reflexonly@gmail.com>
2018-03-02 22:03:40 +0300

    Добавление хелперов и footer

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header
+    if @question.new_record?
+      "Создание нового вопроса для теста #{@question.test.title}"
+    else
+      "Редактирование вопроса для теста #{@question.test.title}"
+    end
+  end
 end
