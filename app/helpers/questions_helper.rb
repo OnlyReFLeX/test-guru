1803319fc2f6ec7a8322296e87b0913c16779bdc
OnlyReFLeX <reflexonly@gmail.com>
2018-03-04 23:25:46 +0300

    edit question header

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,9 @@
 module QuestionsHelper
-  def question_header
-    if @question.new_record?
-      "Создание нового вопроса для теста #{@question.test.title}"
+  def question_header(question)
+    if question.new_record?
+      "Создание нового вопроса для теста #{question.test.title}"
     else
-      "Редактирование вопроса для теста #{@question.test.title}"
+      "Редактирование вопроса для теста #{question.test.title}"
     end
   end
 end
