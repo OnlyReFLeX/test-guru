328ea75db4fd9781f9a9eee48c2b76ebdc148b01
OnlyReFLeX <reflexonly@gmail.com>
2018-03-04 23:41:04 +0300

    delete find_test for index action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy edit update]
-  before_action :find_test, only: %i[index create new]
+  before_action :find_test, only: %i[create new]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def show
