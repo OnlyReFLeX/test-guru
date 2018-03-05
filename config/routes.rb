e59f0ff16b163de3ea10c564f280abfedf91a2e2
OnlyReFLeX <reflexonly@gmail.com>
2018-03-05 21:07:06 +0300

    fix route index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
