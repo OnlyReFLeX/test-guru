c365a3bcdbc62985a6e299fe251ef0d1bf2ec524
OnlyReFLeX <reflexonly@gmail.com>
2018-03-08 16:08:44 +0300

    drop table tests_users

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,8 +8,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
   has_many :questions
-  has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :test_passages
+  has_many :users, through: :test_passages
 
   scope :level, ->(level) { where(level: level) }
   scope :level_easy, -> { level(0..1) }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227134248) do
+ActiveRecord::Schema.define(version: 20180308130338) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 20180227134248) do
     t.integer "test_id"
   end
 
+  create_table "test_passages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.integer "current_question_id"
+    t.integer "correct_questions", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
@@ -41,12 +53,6 @@ ActiveRecord::Schema.define(version: 20180227134248) do
     t.integer "category_id"
     t.integer "author_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
-  end
-
-  create_table "tests_users", id: false, force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
-    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
