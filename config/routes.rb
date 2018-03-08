b50d207313617e7b4a7c3796b24f08769c85c635
OnlyReFLeX <reflexonly@gmail.com>
2018-03-08 17:02:14 +0300

    add controller and views answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :answers
+  root 'tests#index'
+
   resources :tests do
-    resources :questions, shallow: true, except: :index
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
