8239dff10159e5cef5bd87e50e51535e7cf8c045
OnlyReFLeX <reflexonly@gmail.com>
2018-03-08 20:11:22 +0300

    add views and models for  test_passage

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,4 +1,5 @@
 class TestPassagesController < ApplicationController
+  before_action :find_test_passage, only: %i[show result update]
 
   def show
 
@@ -9,6 +10,17 @@ class TestPassagesController < ApplicationController
   end
 
   def update
+    @test_passage.accept!(params[:answer_ids])
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
+  end
 
+  private
+
+  def find_test_passage
+    @test_passage = TestPassage.find(params[:id])
   end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -2,4 +2,52 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
+
+  before_validation :before_validation_set_frist_question, on: :create
+  before_update :before_update_set_next_question
+
+
+  def completed?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+    save!
+  end
+
+  def current_question_number
+    test.questions.count - next_questions.count
+  end
+
+  private
+
+  def before_validation_set_frist_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def before_update_set_next_question
+    self.current_question = next_question
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers_count = correct_answers.count
+
+    correct_answers_count == correct_answers.where(id: answer_ids).count &&
+    correct_answers_count == answer_ids.count
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    next_questions.first
+  end
+
+  def next_questions
+    test.questions.order(:id).where('id > ?', current_question.id)
+  end
 end
