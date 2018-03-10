fc813fa26fd1f7ac5536bcf91694ce0072bd0ec3
OnlyReFLeX <reflexonly@gmail.com>
2018-03-10 18:44:26 +0300

    validates for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  EMAIL_FORMAT = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id'
+
+  validates :email, format: EMAIL_FORMAT, uniqueness: true
+  validates :password, presence: true
 
   has_secure_password
 
