aefdd92afb2017b2179d86cd3876e09592213f2d
OnlyReFLeX <reflexonly@gmail.com>
2018-03-10 16:00:29 +0300

    add signup user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  validates :email, :password_digest, presence: true
-
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id'
+
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(created_at: :desc).find_by(test_id: test.id)
