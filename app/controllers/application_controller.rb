ff6e8ae61695189eb7afb0812ee90e6a96adf536
OnlyReFLeX <reflexonly@gmail.com>
2018-03-13 00:39:02 +0300

    admin redirect to admin path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_root_path
+    else
+      super
+    end
+  end
 end
