517961e140870f64eed47e1597e6d249dfc64c19
OnlyReFLeX <reflexonly@gmail.com>
2018-03-13 01:38:13 +0300

    add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :registerable,
          :recoverable,
          :rememberable,
          :trackable,
-         :validatable
+         :validatable,
+         :confirmable
 
   validates :first_name, :last_name, presence: true
 
