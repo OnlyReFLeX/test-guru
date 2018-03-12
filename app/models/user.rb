2d7a81ff1567c8b520a99c0206813ce5fcc64727
OnlyReFLeX <reflexonly@gmail.com>
2018-03-12 17:24:27 +0300

    install devise and letter opener

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
-  EMAIL_FORMAT = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id'
-
-  validates :email, format: EMAIL_FORMAT, uniqueness: true
-  validates :password, presence: true
-
-  has_secure_password
 
   def test_passage(test)
     test_passages.order(created_at: :desc).find_by(test_id: test.id)
